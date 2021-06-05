0359a51c1b84afdee98519490342e60847a3758b
shahryar <shahryar.haghighi.fard@gmail.com>
2021-06-05 17:01:00 -0400

    designed the main container

---
+++ algo-vis/src/resources/components/2_Visualizer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Visualizer extends Component {
+  render() {
+    return (
+      <div className="vizContainer">
+        <h2>Visualaizer2</h2>
+        <h3>Visualaizer2</h3>
+      </div>
+    );
+  }
+}
+
+export default Visualizer;
