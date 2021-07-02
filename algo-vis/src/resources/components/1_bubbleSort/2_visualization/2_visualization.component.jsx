9973ba0d6ca6137722760d9d1543ffad2d6aecf0
shahryar <shahryar.haghighi.fard@gmail.com>
2021-07-01 22:12:08 -0400

    visualizer reduxed

--- algo-vis/src/resources/components/1_bubbleSort/2_visualization/2_visualization.component.jsx
+++ algo-vis/src/resources/components/1_bubbleSort/2_visualization/2_visualization.component.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import InputHandler from "./3_inputHandler/3_inputHandler.component";
 import Status from "./3_status/3_status.component";
 import ControlButtons from "./3_controls/3_ControlButtons";
+import Visualizer from "./3_visualizer/3_Visualizer_component";
 export default function Visualization() {
   return (
     <>
       <InputHandler />
       <Status />
       <ControlButtons />
+      <Visualizer />
     </>
   );
 }
---
+++ algo-vis/src/resources/components/1_bubbleSort/2_visualization/3_visualizer/3_Visualizer_component.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const Visualizer = ({ mainSchema, step }) => {
+  return (
+    <div className="sortItems">
+      {mainSchema[step]?.map((item, index) => (
+        <div
+          key={`itemBox-${item.sortPosiotion}-${index}`}
+          className={
+            item.selected
+              ? `sortItem sortItem${item.sortPosiotion} selected`
+              : `sortItem sortItem${item.sortPosiotion}`
+          }
+        >
+          {item.userInput}
+        </div>
+      ))}
+    </div>
+  );
+};
+const mapStateToProps = (state) => ({
+  mainSchema: state.mainSchema.mainSchema,
+  step: state.step.step,
+});
+
+export default connect(mapStateToProps)(Visualizer);
