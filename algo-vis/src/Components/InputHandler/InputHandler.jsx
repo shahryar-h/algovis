25c791ddf8b3516bf4c325cc1ec6376e2771e134
Shahryar <shahryar.haghighifard@easy1booking.com>
2021-11-23 16:41:41 -0500

    submit input renamed

--- algo-vis/src/Components/InputHandler/InputHandler.jsx
+++ algo-vis/src/Components/InputHandler/InputHandler.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "./4_box/4_box.component";
-import SubmitInput from "./4_submitInput/4_submitInput.component";
+import SubmitInput from "./SubmitInput/SubmitInput.component";
 // import DisplayError from "./4_displayerror/4_displayError.component";
 // import ControlButtons from "../3_controls/3_ControlButtons";
 
