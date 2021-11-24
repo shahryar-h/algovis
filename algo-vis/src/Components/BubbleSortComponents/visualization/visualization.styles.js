1b12ad9efa1a8e323005b7d749999972e7161a43
Shahryar <shahryar.haghighifard@easy1booking.com>
2021-11-24 14:41:54 -0500

    style refactoring

--- algo-vis/src/Components/BubbleSortComponents/visualization/visualization.styles.js
+++ algo-vis/src/Components/BubbleSortComponents/visualization/visualization.styles.js
@@ -1,19 +1,14 @@
 import styled from "styled-components/macro";
 
-const VisContainer = styled.div`
+export const VisContainer = styled.div`
   height: 100%;
-  /* display: grid;
-  grid-auto-columns: 1fr;
-  grid-template-columns: 1fr;
-  grid-template-rows: 35% 65%; */
   display: flex;
   flex-direction: column;
   justify-content: center;
 `;
-const ControlAndStatus = styled.div`
+export const ControlAndStatus = styled.div`
   width: 80%;
   margin: 0px auto;
-  /* background: blue; */
   height: 35%;
   display: grid;
   grid-auto-columns: 1fr;
@@ -21,4 +16,8 @@ const ControlAndStatus = styled.div`
   gap: 0px 1em;
 `;
 
-export { VisContainer, ControlAndStatus };
+export const InputWrapper = styled.div`
+  display: "flex";
+  flex-direction: "column";
+  justify-content: "space-evenly";
+`;
